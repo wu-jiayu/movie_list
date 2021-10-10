96830447e5f9ef392341c517cdc9f4c4fbb438ee
JiaYu Wu <jiayu.wu666@gmail.com>
2021-10-10 09:49:46 +0800

    finish the tutorials

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const exphbs = require("express-handlebars")
+const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
 const port = 3000
 
 // express template engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 // setting static files
-app.use(express.static("public"))
+app.use(express.static('public'))
 
-// routes setting
-app.get("/", (req, res) => {
-  res.render("index")
+// routes setting:
+app.get('/', (req, res) => {
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/search', (req, res) => {
+  console.log('req keyword', req.query.keyword)
+  const movies = movieList.results.filter((movie) => {
+    // movie.title include req.query.keyword
+    return movie.title
+      .toLowerCase()
+      .includes(req.query.keyword.toLocaleLowerCase())
+  })
+  res.render('index', { movies: movies, keyword: req.query.keyword })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movie = movieList.results.find(
+    (movie) => movie.id.toString() === req.params.movie_id
+  )
+  res.render('show', { movie: movie })
 })
 
 // start and listen on the express server
